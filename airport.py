7b36625d152365f6d308fa021bc8d5b71e132005
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-02-08 23:34:35 +0300

    Update airport.py
    добавлен в оъбект client

--- airport.py
+++ airport.py
@@ -4,10 +4,12 @@ import paho.mqtt.client as mqtt
 from threading import Thread
 import time
 import os
-TOKEN = "1020204517:AAFYVzNcIg4DfT7aUGJjOx91ae8XVGXNHhg"
+TOKEN = os.environ['TOKEN']
 bot = telebot.TeleBot(TOKEN)
 
 mqtt_callback = 10
+print(TOKEN)
+
 
 @bot.message_handler(commands=['start', 'go'])
 def send_welcome(message):
@@ -23,8 +25,8 @@ def keyboard():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     btn1 = types.KeyboardButton('Engine') #объявляем кнопку
     btn2 = types.KeyboardButton('Floor')
-    markup.add(btn1) #задаем кнопки, чере запятую
-    markup.add(btn2)
+    markup.add(btn1, btn2) #задаем кнопки, чере запятую
+   #markup.add(btn2)
     return markup
 
 @bot.message_handler(content_types=["text"]) #принимает любой текст фигню какую-то
@@ -46,7 +48,8 @@ def send_anytext(message):     #обратная связь, после полу
                         text = 'выключен'
                         bot.send_message(chat_id, text, parse_mode='HTML', reply_markup=keyboard())
                         client.publish("/airport_callback", payload="0", qos=0, retain=False)
-                    global chat_id_for_auto = chat_id
+                client.chat_idG = chat_id
+
 
         if message.text == 'Floor':
             client.publish("/airport", payload="on_floor", qos=0, retain=False)
@@ -62,8 +65,7 @@ def send_anytext(message):     #обратная связь, после полу
                         text = 'выключен'
                         bot.send_message(chat_id, text, parse_mode="HTML", reply_markup=keyboard())
                         client.publish("/airport_callback", payload="0", qos=0, retain=False)
-        #    text = 'Подогрвев пола ВКЛЮЧЕН'
-         #   bot.send_message(chat_id, text, parse_mode='HTML', reply_markup=keyboard())
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -77,17 +79,12 @@ def on_message(client, userdata, msg,): #(client, userdata, msg)
     mqtt_callback = msg.payload
    # callback_msg = (msg.payload)
 
-def check_upd(client, message):
-    run1 = True 
-    while run1:
+def check_upd(client):
+    while True:
         if mqtt_callback == b'engine_is_off_auto':
-            print("функция побежала")
             client.publish("/airport_callback", payload="0", qos=0, retain=False)
-            text = 'Выключен подогрев двигателя по истечению времени'
-            #chat_id = '441494356'
-            bot.send_message(message.chat.id, text)
-        run1 = True
-
+            text = 'Автоматически выключен подогрев двигателя'
+            bot.send_message(client.chat_idG, text)
 
 
 client = mqtt.Client()
@@ -96,9 +93,9 @@ client.on_message = on_message
 client.username_pw_set("vcnpayei","Mc55q9zvQ7Ek")
 client.connect("farmer.cloudmqtt.com", 12415, 60)
 
-#Thread(target=bot.polling, args=(True,)).start()
+
 Thread(target=client.loop_forever, args=()).start()
-Thread(target=check_upd, args=(client, chat_id_for_auto,)).start()
+Thread(target=check_upd, args=(client,)).start()
 #bot.polling(none_stop=True)  # bot.infinity_polling(True). если бот будет падать, то поставить это
 
 while True:
