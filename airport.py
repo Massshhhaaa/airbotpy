133e2c93343725241f49e2be36e3bf3a18be29c1
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-06-11 03:16:01 +0300

    Update airport.py

--- airport.py
+++ airport.py
@@ -108,7 +108,8 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("/airport_callback", "/airport_sensor")
+    client.subscribe("/airport_callback")
+    client.subscribe("/airport_sensor")
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg,): #(client, userdata, msg)
     print(msg.topic+" "+str(msg.payload))
