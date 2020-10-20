4319ce1a5b90c4793e0079747d2693ae94656e31
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-10-21 00:38:54 +0300

    Update airport.py

--- airport.py
+++ airport.py
@@ -61,9 +61,8 @@ def operation(type_operation, message):
     while True:
         if mqtt_callback == b'engine_is_on':
             d = datetime.now()
-            d.strftime("%d %b %H:%M")
             filework(1, 'heat off engine\n')
-            cb_msg = "Подогрев двигателя включен "+str(d)+ "\nЗапланированное выключение через 3 часа"
+            cb_msg = "Подогрев двигателя включен "+str(d.strftime("%d %b %H:%M"))+ "\nЗапланированное выключение через 3 часа"
             chat_idG = message.chat.id
             break
 
