61e239cea13602ae5f3c75a5f1d0c0a550957d82
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-10-20 23:20:06 +0300

    minor

--- airport.py
+++ airport.py
@@ -8,6 +8,7 @@ import paho.mqtt.client as mqtt
 from threading import Thread
 import time
 import os
+from operations import operation, security_operations
 
 bot = telebot.TeleBot(os.environ['TOKEN'])
 mqtt_callback = 10
@@ -21,51 +22,6 @@ def send_welcome(message):
         '''Добро пожаловать, Милорд.
         ''',
         reply_markup=keyboard())
-
-
-def operation(type_operation, message):
-    global chat_idG
-    chat_idG = message.chat.id
-    timeout = 5
-    t1 = datetime.now()
-
-    client.publish("/airport", payload=type_operation, qos=0, retain=False)
-    while True:
-        if mqtt_callback == b'engine_is_on':
-            filework(1, 'heat off engine\n')
-            cb_msg = "ВКЛЮЧЕН"
-            chat_idG = message.chat_id
-            break
-
-        elif mqtt_callback == b'engine_is_off':
-            filework(1, 'heat on engine\n')
-            cb_msg = 'выключен'
-            break
-
-        elif mqtt_callback == b'floor_is_on':
-            filework(2, 'heat off floor\n')
-            cb_msg = 'включен'
-            break
-
-        elif mqtt_callback == b'floor_is_off':
-            filework(2, 'heat on floor\n')
-            cb_msg = 'выключен'
-            break
-
-        elif (datetime.now()-t1).seconds > timeout:
-            cb_msg = 'Нет соединения'
-            break
-
-    client.publish("/airport_callback", payload="0", qos=0, retain=False)
-    bot.send_message(message.chat_id, text = cb_msg, parse_mode="HTML", reply_markup=keyboard())
-
-
-def security_operations(chat_id, payload, btn_status):
-    client.publish("/airport_callback", payload="0", qos=0, retain=False)
-    client.publish("/airport", payload=payload, qos=0, retain=False)
-    filework(0, btn_status)
-    bot.send_message(chat_id, text='oк', parse_mode='HTML', reply_markup=keyboard())
-
 
 
 def filework(place, text): # place = 0-2 in list, text must be include "something\n"
@@ -106,10 +62,10 @@ def send_anytext(message):                                                      
         operation("on_floor", message)
 
     if message.text == 'activate security':
-        security_operations(message.chat_id, payload="security_activated", btn_status="deactivate\n")
+        security_operations(message.chat.id, payload="security_activated", btn_status="deactivate\n")
 
     if message.text == 'deactivate':
-        security_operations(message.chat_id, payload="security_deactivated", btn_status="activate security\n")
+        security_operations(message.chat.id, payload="security_deactivated", btn_status="activate security\n")
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
