b93036197a0ae87765076132a0f08fb64ebffec6
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-12-12 12:30:44 +0300

    Update auto_heating_air.ino

--- auto_heating_air/auto_heating_air.ino
+++ auto_heating_air/auto_heating_air.ino
@@ -1,108 +1,170 @@
-#include <MsTimer2.h>
-#include <MQTT.h>
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
-#include "Wire.h"
 
 #define BUFFER_SIZE 100
-#define RELAY_PIN1 12 //розетка на двигатель(правая)
-#define RELAY_PIN2 13  // HIGH соотвествует выключенному положение реле
-#define SENSOR_PIN1 5
+#define RELAY_PIN1 D6 //розетка на двигатель(правая)
+#define RELAY_PIN2 D7  // HIGH соотвествует выключенному положение реле
+#define SENSOR_PIN1 D4
 
-// топик /airport используется для отправки с бота на контроллер запросов
-// /airport_callback используется для отправки с контроллера на сервер ответов
-// /airport_sensor для результатов датчика движения
-//
+#define ENGINEHEATING "/engine_heating"
+#define FLOORHEATING "/floor_heating"
+#define MOTION_SENSOR "/motion_sensor"
 
-const char *ssid =  "TP-Link_E82C";  // Имя вайфай точки доступа
-const char *pass =  "11774372"; // Пароль от точки доступа
+#define DATA_MOTION_SENSOR "/motion_sensor/data"
 
-//const char *ssid =  "Mahsa Naumova";  // Имя вайфай точки доступа
-//const char *pass =  "masha1500"; // Пароль от точки доступа
+#define MOTION_SENS 7 //число обнаруженных движения для функции отправки
+#define MOTION_SENS_TIME 50
+
+//const char *ssid =  "TP-Link_E82C"; 
+//const char *pass =  "11774372"; 
+
+const char *ssid =  "WiFi-DOM.RU-7630"; 
+const char *pass =  "JVLuSoUA"; 
 
 bool off_rstflag = true;
 bool automate_disable_flag = false;
-bool security_condition = false; //true is on
 
-const char *mqtt_server = "farmer.cloudmqtt.com"; // Имя сервера MQTT
-const int mqtt_port = 12415; // Порт для подключения к серверу MQTT
-const char *mqtt_user = "vcnpayei"; // Логин от сервер
-const char *mqtt_pass = "Mc55q9zvQ7Ek"; // Пароль от сервера
-const int delayfor = 15;
+const char *mqtt_server = "farmer.cloudmqtt.com"; 
+ const int mqtt_port = 12415; 
+ const char *mqtt_user = "vcnpayei";
+ const char *mqtt_pass = "Mc55q9zvQ7Ek"; 
 
-uint32_t timestart;
-uint32_t timeconst = 10800000; // врямя автоматического отключения 3 часа в мс
+uint32_t motionT = MOTION_SENS_TIME;  
+uint32_t engineT;
+uint32_t timeconst = 10800000; // врямя автоматического отключения 3 часа
 
-int number = 0;
+
+ int t = millis();
+
+ volatile int count = 0;
+ volatile int ledState = LOW; //Define ledOut, default is off
+
 
 WiFiClient wclient;
 PubSubClient client(wclient, mqtt_server, mqtt_port);
 
+
+ class Element {
+  public: 
+      String topic;
+      String backTopic;
+      int pin;
+      bool state = false;
+      
+     Element(int initPin, String initTopic){
+     topic = initTopic;
+     pin = initPin;
+     backTopic = topic + "/back";
+    }
+
+        
+        void enable() {
+                state = true;
+                digitalWrite(pin, LOW);
+                client.publish(backTopic, String("1"));
+                Serial.println("pin:  enabled");
+
+                if (topic == "/engine_heating"){automate_disable_flag = true; engineT = millis();}
+              }
+
+    
+         void disable() {
+             state = false;
+              digitalWrite(pin, HIGH);
+              client.publish(backTopic, String("0"));
+              Serial.println("pin: disabled");
+            }
+              
+            
+      void main(String payload, String pubTopic){
+           
+          if (topic == pubTopic) {
+          Serial.println("topic совпадает");
+          
+              if (payload == "1") {enable();}
+              if (payload == "0") {disable();}
+
+          Serial.print("STATE: ");Serial.println(state);
+         }
+
+      
+  
+  } 
+          
+};
+
+class Sensor : public Element{
+  public:
+    Sensor(int initPin, String initTopic):Element(initPin, initTopic){};
+        
+      void process() {
+        delay(1);
+        if( count > MOTION_SENS && millis() - motionT < MOTION_SENS_TIME) {
+            ledState = HIGH;
+            digitalWrite(2, state);
+            client.publish(DATA_MOTION_SENSOR, String("1"));
+            Serial.println("Сообщение об обнаружении движения отправлено");
+            count=0;        
+       } else{ count=0; }
+
+      }     
+};
+
+  Element engineHeating(RELAY_PIN1, ENGINEHEATING), floorHeating(RELAY_PIN2, FLOORHEATING);
+
+  Sensor motionSensor(SENSOR_PIN1, ENGINEHEATING);
+  
+    
 void callback(const MQTT::Publish & pub) {     // Функция получения данных от сервера
   Serial.print(pub.topic());                // выводим в сериал порт название топика
   Serial.print(" => ");
   Serial.println(pub.payload_string());     // выводим в сериал порт значение полученных данных
-
+  
   String payload = pub.payload_string();
-  if (String(pub.topic()) == "/airport") {  //  проверяем из нужного ли нам топика пришли данные
+  String pubTopic = pub.topic();
 
-    if (payload == "on_engine") {
-      if (digitalRead(RELAY_PIN1) == HIGH) {
-        digitalWrite(RELAY_PIN1, LOW);
-        client.publish("/airport_callback", String("engine_is_on"));
-      } else {
-        digitalWrite(RELAY_PIN1, HIGH);
-        client.publish("/airport_callback", String("engine_is_off"));
-      }
-      timestart = millis();
-      automate_disable_flag = true;
-    }
+      
+  engineHeating.main(payload, pubTopic);
+  floorHeating.main(payload, pubTopic);
+  motionSensor.main(payload, pubTopic);
 
-    if (payload == "on_floor") {
-      if (digitalRead(RELAY_PIN2) == HIGH) {
-        digitalWrite(RELAY_PIN2, LOW);
-        client.publish("/airport_callback", String("floor_is_on"));
-      } else {
-        digitalWrite(RELAY_PIN2, HIGH);
-        client.publish("/airport_callback", String("floor_is_off"));
-      }
-    }
-
-    if (payload == "security_activated") {
-      security_condition = true;
-      client.publish("/airport_callback", String("now_security_activate"));
-    }
-
-    if (payload == "security_deactivated") {
-      security_condition = false;
-      client.publish("/airport_callback", String("now_security_deactive"));
-    }
-
-  }
+      
 }
 
+
+ICACHE_RAM_ATTR void blinks() {
+  Serial.println("void blink() - движение опознано");
+  count++;
+  motionT = millis();
+}
+
+
+
+
 void setup() {
+  blinks();
+  
   Serial.begin(115200);
   pinMode(RELAY_PIN1, OUTPUT);
   pinMode(RELAY_PIN2, OUTPUT);
 
-  attachInterrupt(SENSOR_PIN1, stateChange, FALLING); // Set the interrupt function, interrupt pin is digital pin D2, interrupt service function is stateChange (), when the D2 power change from high to low , the trigger interrupt.
-  MsTimer2::set(500, Handle); // Set the timer interrupt function, running once Handle() function per 1000ms
-  MsTimer2::start();//Start timer interrupt function
-
+  pinMode (LED_BUILTIN, OUTPUT);
+  pinMode (SENSOR_PIN1, INPUT_PULLUP);
+  
+//  –Задаем  функцию blink , которая будет вызвана по внешнему прерыванию.
+  attachInterrupt ( digitalPinToInterrupt (SENSOR_PIN1), blinks, CHANGE);
 
 }
 
+
 void loop() {
 
-  AutoDisableRestasrt();
   // подключаемся к wi-fi
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Connecting to ");
     Serial.print(ssid);
     Serial.println("...");
     WiFi.begin(ssid, pass);
-
     if (WiFi.waitForConnectResult() != WL_CONNECTED)
       return;
     Serial.println("WiFi connected");
@@ -116,49 +178,130 @@ void loop() {
                          .set_auth(mqtt_user, mqtt_pass))) {
         Serial.println("Connected to MQTT server");
         client.set_callback(callback);
-        client.subscribe("/airport");      // подписывааемся по топик с данными
-      } else {
-        Serial.println("Could not connect to MQTT server");
+
+        client.subscribe(ENGINEHEATING);
+        client.subscribe(FLOORHEATING);  
+        client.subscribe(MOTION_SENSOR);    
+        
       }
     }
-
+  
     if (client.connected()) {
-
+    
       client.loop();
-      AutoDisableTimeLimit();
+      //автоматическое флажковое отключение при рестарте
+      if (off_rstflag == true) {
 
+        Serial.println("Рестарт..Выключение реле..");
+        engineHeating.disable();
+        floorHeating.disable();
+        off_rstflag = false;
+      }
+      
+        //отключение при достижении лимита времени подогрева двигателя
+       if (digitalRead(RELAY_PIN1) == LOW && millis() - engineT >= timeconst && automate_disable_flag == true){
+          engineHeating.disable();
+          automate_disable_flag = false;
+       }
+           
+
+           
     }
+    motionSensor.process();
+    
+   
   }
-
+  delay(1);
 }
 
 
-void stateChange() {
-  number++;  //Interrupted once, the number + 1
-}
 
-void Handle() { //Timer service function
-  if ((number > 1) && (security_condition == true )) { //If in the set of the interrupt time the number more than 1 times, then means have detect moving objects,This value can be adjusted according to the actual situation, which is equivalent to adjust the threshold of detection speed of moving objects.
-    client.publish("/airport_sensor", String("motion_detected"));
-    number = 0; //Cleare the number, so that it does not affect the next trigger
-  }
-  else
-    number = 0;
-}
+//http://arduino.ru/forum/obshchii/datchik-hb100-kak-opredelit-nalichie-dvizheniya
+//-----Значение readMean и readMedian нужно подбирать в зависимости от зашумленности...
 
-void AutoDisableTimeLimit () {  //отключение при достижении лимита времени подогрева двигателя
-  if ((digitalRead(RELAY_PIN1) == LOW) && (millis() - timestart >= timeconst) && (automate_disable_flag == true)) {
-    digitalWrite(RELAY_PIN1, HIGH);
-    client.publish("/airport_callback", String("engine_is_off_auto"));
-    automate_disable_flag = false;
-  } delay(delayfor);
-}
+//
+//const int ledPin =  7;      
+//int ledState = LOW; 
+//long previousMillis = 0;
+//long interval = 1000;
+//
+//int sensorPin = A0; // номер аналогового входа
+//
+//// функция считывает аналоговый вход заданное количество раз
+//// и возвращает отфильтрованное значение
+//int readMean(int pin, int samples){
+//  // переменная для хранения суммы считанных значений
+//  int sum = 0;
+//  // чтение и складывание значений
+//  for (int i = 0; i < samples; i++){
+//    sum = sum + analogRead(pin);
+//  }
+//  // делим сумму значений на количество измерений
+//  sum = sum/samples;
+//  // возвращаем среднее значение
+//  return sum;
+//}
+//
+//// функция считывает аналоговый вход заданное количество раз
+//// и возвращает медианное отфильтрованное значение
+//int readMedian (int pin, int samples){
+//  // массив для хранения данных
+//  int raw[samples];
+//  // считываем вход и помещаем величину в ячейки массива
+//  for (int i = 0; i < samples; i++){
+//    raw[i] = analogRead(pin);
+//  }
+//  // сортируем массив по возрастанию значений в ячейках
+//  int temp = 0; // временная переменная
+//
+//  for (int i = 0; i < samples; i++){
+//    for (int j = 0; j < samples - 1; j++){
+//      if (raw[j] > raw[j + 1]){
+//        temp = raw[j];
+//        raw[j] = raw[j + 1];
+//        raw[j + 1] = temp;
+//      }
+//    }
+//  }
+//  // возвращаем значение средней ячейки массива
+//  return raw[samples/2];
+//}
+//void setup(){
+//  Serial.begin(9600);
+//  pinMode(7, OUTPUT); 
+//}
+//void loop(){
+//  // выводим значение на аналоговом входе в монитор порта
+//  Serial.print(analogRead(sensorPin));
+//  Serial.print(" ");
+//  // выводим среднеизмеренное значение
+//  Serial.print(readMean(sensorPin, 15));
+//  Serial.print(" ");
+//  // выводим медианное отфильтрованное значение
+//  Serial.println(readMedian(sensorPin, 15));
+//  delay(100);
+//  if ((readMean(sensorPin, 15) < 140 || readMean(sensorPin, 15)>850 ) && (readMedian(sensorPin, 15)< 140 || readMedian(sensorPin, 15)> 850) ){
+//    ledState=HIGH;      
+//    digitalWrite(ledPin, ledState);
+//  }
+//   unsigned long currentMillis = millis();  
+//         if(currentMillis - previousMillis > interval) {
+//       previousMillis = currentMillis; 
+//    if (ledState == HIGH)
+//      ledState = LOW;
+//    digitalWrite(ledPin, ledState);
+//         }
+//  }
+//
+//
+//
+//
+//
+//
 
 
-void AutoDisableRestasrt () { //автоматическое флажковое отключение при рестарте
-  if (off_rstflag == true) {
-    digitalWrite(RELAY_PIN1, HIGH);
-    digitalWrite(RELAY_PIN2, HIGH);
-    off_rstflag = false;
-  } delay(delayfor);
-}
+
+
+
+
+
